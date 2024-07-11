3a3ace1ef811bed04302f122e1428831b19324bb
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-07-11 13:51:03 +0500

    Project update

--- app/animation/page.tsx
+++ app/animation/page.tsx
@@ -14,7 +14,7 @@ function ProductImages() {
     loop: true,
     from: { transform: 'translateX(-100%)' },
     to: { transform: 'translateX(100%)' }, 
-    config: { duration: 5000 }
+    config: { duration: 3000 }
   });
 
   return (
