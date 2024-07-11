f2f78976213ed40f91742cf4ce401ffbfe2a4db9
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-07-11 13:31:14 +0500

    project update

--- components/card/page.tsx
+++ components/card/page.tsx
@@ -1,70 +1,3 @@
-// // components/card/page.tsx
-// "use client"
-// import { DeleteOutlined, HeartOutlined } from '@ant-design/icons';
-// import React, { useState } from 'react';
-// import './style.css';
-// import useCartsStore from '@/store/card/page'; // Check this path
-// import { toast } from 'react-toastify';
-// import Cookies from 'js-cookie';
-// import uselikestore from '@/store/likes/page';
-// import Image from 'next/image';
-// import { Rate } from 'antd';
-// import useCartItemsStore from '@/store/cartItemStore/page'; // Check this path
-
-// function CardComponent({ datas }: any) {
-//   const [count, setCount] = useState(1);
-//   const { deleteCards, getCards } = useCartsStore(); // Check if deleteCards and getCards are correctly implemented
-//   const { postlikes, getlikes } = uselikestore(); // Check if postlikes and getlikes are correctly implemented
-//   const { updateCartItem }: any = useCartItemsStore(); // Check this path
-
-//   async function handleDelete(id: number) {
-//     const response = await deleteCards({ id });
-//     if (response?.status == 200) {
-//       toast.success('Card deleted');
-//       await getCards({ id: Number(Cookies.get('id')) });
-//     } else {
-//       toast.error('Card deletion failed');
-//     }
-//   }
-
-//   async function handleLike(id: number) {
-//     const response = await postlikes({ product_id: id });
-//     if (response.status == 201) {
-//       await getlikes({ id: Number(Cookies.get('id')) });
-//       toast.success('Like successful');
-//     } else {
-//       toast.error('Like failed. You need to login');
-//     }
-//   }
-
-//   const handleQuantityChange = (newCount: number) => {
-//     setCount(newCount);
-//     updateCartItem(datas.id, newCount); // Update the global store
-//   };
-
-//   return (
-//     <div className='product-card justify-between'>
-//       <Image className='product-image' src={datas?.product_id?.images[0]} alt={datas?.product_id?.name} width={100} height={100} />
-//       <div className='product-details'>
-//         <p className='product-title'>{datas?.product_id?.name}</p>
-//         <div className='quantity-controls'>
-//           <button onClick={() => handleQuantityChange(count - 1)} disabled={count === 1} className='quantity-btn'>-</button>
-//           <p className='quantity'>{count}</p>
-//           <button onClick={() => handleQuantityChange(count + 1)} className='quantity-btn2'>+</button>
-//         </div>
-//       </div>
-//       <div className='product-actions'>
-//         <div className='action-icons'>
-//           <span className='product-price float-left '>{`${datas?.product_id?.price} soums`}</span>
-//           <span onClick={() => handleDelete(datas?.id)} className='action-icon mt-[100px]'><DeleteOutlined className='icon' /></span>
-//           <span onClick={() => handleLike(datas?.product_id?.id)} className='action-icon mt-[100px]'><HeartOutlined className='icon' /></span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default CardComponent;
 "use client"
 
 import { DeleteOutlined, HeartOutlined } from '@ant-design/icons';
@@ -114,7 +47,7 @@ function Page({datas}:any) {
                 </div>
             </div>
             <div className='product-actions'>
-                <h4 className='product-price'>{datas?.product_id?.price * count} so‘m</h4>
+                <h4 className='product-price'>{datas?.product_id?.price * count} soums</h4>
                 <div className='action-icons'>
                     <span onClick={() => handleDelete(datas?.id)} className='action-icon'><DeleteOutlined className='icon' /></span>
                     <span onClick={() => handleLike(datas?.product_id?.id)} className='action-icon'><HeartOutlined className='icon' /></span>
